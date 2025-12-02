ae689edc0fe4288741ca8da0cb557bc3c1bb7a6a
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-12-02 10:34:37 +0530

    No messages Wali SCreen

--- src/pages/Pager.js
+++ src/pages/Pager.js
@@ -160,6 +160,14 @@ function Messages() {
     };
   }, []);
 
+  if (!messages?.length) {
+    return (
+      <div className="px-5 py-5 flex items-center justify-center h-full">
+        <p className="text-white">No messages yet. Start the conversation!</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       {messages.map((message, index) => {
